b1b7ae0ce1aa8378f7f04328b2252370d45b5d0e
Richard Cornish <richardcornish@gmail.com>
2014-04-15 04:01:40 -0500

    Fixed path names for static and media root directories

--- cyndiloza/settings/base.py
+++ cyndiloza/settings/base.py
@@ -53,7 +53,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = '/home/richardcornish/webapps/cyndilozacom_assets/media/'
+MEDIA_ROOT = '/home/richardcornish/webapps/cyndiloza_assets/media/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -64,7 +64,7 @@ MEDIA_URL = 'http://assets.cyndiloza.com/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/home/richardcornish/webapps/cyndilozacom_assets/static/'
+STATIC_ROOT = '/home/richardcornish/webapps/cyndiloza_assets/static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
