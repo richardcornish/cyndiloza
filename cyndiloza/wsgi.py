b012128487e4e83a8dccbde290d69da5d68b9835
Richard Cornish <richardcornish@gmail.com>
2014-04-16 04:17:54 -0500

    Cosmetic cleaning of WSGI file

--- cyndiloza/wsgi.py
+++ cyndiloza/wsgi.py
@@ -1,7 +1,14 @@
-import os
+"""
+WSGI config for hi project.
 
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
+"""
+
+import os
 os.environ["DJANGO_SETTINGS_MODULE"] = os.environ["CYNDILOZA_DSM"]
 
 from django.core.wsgi import get_wsgi_application
-
 application = get_wsgi_application()
