4c889128a76155d0fbcd9d151b50b3e6a80f1f8b
Richard Cornish <richardcornish@gmail.com>
2014-04-17 18:53:49 -0500

    Renaming view class to something that makes sense for list views

--- cyndiloza/apps/about/views.py
+++ cyndiloza/apps/about/views.py
@@ -3,9 +3,9 @@ from django.views.generic import ListView
 from cyndiloza.apps.about.models import About
 
 
-class AboutDetailView(ListView):
+class AboutListView(ListView):
     queryset = About.objects.all()[0]
     context_object_name = 'about'
     template_name = 'about/about_list.html'
 
-about_list = AboutDetailView.as_view()
+about_list = AboutListView.as_view()
