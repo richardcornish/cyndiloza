3414795c6094e493dd4180b001a069df6653b455
Richard Cornish <richardcornish@gmail.com>
2014-04-17 18:44:45 -0500

    Fixed broken about page, updated dependencies

--- cyndiloza/apps/about/urls.py
+++ cyndiloza/apps/about/urls.py
@@ -3,10 +3,8 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('cyndiloza.apps.about.views',
 
-    # About detail
-    # No need for slug because we always have one "about" entry
     url(r'^$',
-        view='about_detail',
+        view='about_list',
         name='about_about_detail'
     ),
 
--- cyndiloza/apps/about/views.py
+++ cyndiloza/apps/about/views.py
@@ -1,9 +1,11 @@
-from django.views.generic import DetailView
+from django.views.generic import ListView
 
 from cyndiloza.apps.about.models import About
 
 
-class AboutDetailView(DetailView):
-    model = About
+class AboutDetailView(ListView):
+    queryset = About.objects.all()[0]
+    context_object_name = 'about'
+    template_name = 'about/about_list.html'
 
-about_detail = AboutDetailView.as_view()
+about_list = AboutDetailView.as_view()
