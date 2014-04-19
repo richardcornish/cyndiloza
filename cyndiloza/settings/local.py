ee780d0f9d498ed1f8d6069c07aa576f4a2c8667
Richard Cornish <richardcornish@gmail.com>
2014-04-19 16:21:20 -0500

    Added dummy caching to local development

--- cyndiloza/settings/local.py
+++ cyndiloza/settings/local.py
@@ -17,6 +17,12 @@ STATIC_ROOT = os.path.join(REPOSITORY_ROOT, 'assets', 'static')
 
 STATIC_URL = '/static/'
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+    }
+}
+
 THUMBNAIL_DUMMY = True
 
 THUMBNAIL_DUMMY_SOURCE = 'http://placehold.it/%(width)sx%(height)s'
